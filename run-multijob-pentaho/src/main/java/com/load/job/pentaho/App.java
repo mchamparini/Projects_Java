a1563c7a8fb087ee53fa4c3a0b611c6366907943
mchamparini <martin.champarini@gmail.com>
2016-12-14 07:16:30 -0300

    14-12-2016

--- run-multijob-pentaho/src/main/java/com/load/job/pentaho/App.java
+++ run-multijob-pentaho/src/main/java/com/load/job/pentaho/App.java
@@ -1,13 +1,25 @@
 package com.load.job.pentaho;
 
-/**
- * Hello world!
- *
- */
+import org.pentaho.di.core.KettleEnvironment;
+import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.trans.Trans;
+import org.pentaho.di.trans.TransMeta;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	 try {
+             KettleEnvironment.init();
+             TransMeta metaData = new TransMeta("/home/osboxes/Documents/first_transformation.ktr");
+             Trans trans = new Trans( metaData );
+             trans.execute( null );
+             trans.waitUntilFinished();
+             if ( trans.getErrors() > 0 ) {
+                 System.out.print( "Error Executing transformation" );
+             }
+         } catch( KettleException e ) {
+             e.printStackTrace();
+         }
     }
 }
