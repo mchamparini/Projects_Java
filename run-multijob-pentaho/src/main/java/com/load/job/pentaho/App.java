f02d14a8561ba7f476b0589c5cfac1189be73a05
mchamparini <martin.champarini@gmail.com>
2016-12-13 23:21:02 -0300

    13-12-2016

---
+++ run-multijob-pentaho/src/main/java/com/load/job/pentaho/App.java
@@ -0,0 +1,13 @@
+package com.load.job.pentaho;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
